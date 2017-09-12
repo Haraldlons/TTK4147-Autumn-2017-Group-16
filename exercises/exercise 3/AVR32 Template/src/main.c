9ee00ed2dcc52a374d53887c8ac200885c629274
unknown <pederhb@itk-d669.win.ntnu.no>
2017-09-12 12:47:02 +0200

    Finished exercise 3

--- exercises/exercise 3/AVR32 Template/src/main.c
+++ exercises/exercise 3/AVR32 Template/src/main.c
@@ -37,7 +37,7 @@ void init()
 	INTC_register_interrupt(&interrupt_J3,   AVR32_GPIO_IRQ_3, AVR32_INTC_INT1);
 	cpu_irq_enable();
 
-	//  stdio init
+	//  stdio init 
 	stdio_usb_init(&CONFIG_USART_IF);
 
 	// Specify that stdout and stdin should not be buffered.
@@ -55,10 +55,26 @@ User decelerations
 /*********************************************************************
 Interrupt routines
 *********************************************************************/
+
+int Aflag = 0;
+int Bflag = 0;
+int Cflag = 0;
+	
 __attribute__((__interrupt__)) 
 static void interrupt_J3(void) 
 { 
-
+	if(gpio_get_pin_interrupt_flag(TEST_A)){
+		gpio_clear_pin_interrupt_flag(TEST_A);
+		Aflag = 1;
+	}
+	if(gpio_get_pin_interrupt_flag(TEST_B)){
+		gpio_clear_pin_interrupt_flag(TEST_B);
+		Bflag = 1;
+	}
+	if(gpio_get_pin_interrupt_flag(TEST_C)){
+		gpio_clear_pin_interrupt_flag(TEST_C);
+		Cflag = 1;
+	}
 }
 
 /*********************************************************************
@@ -66,16 +82,35 @@ Functions
 *********************************************************************/
 int main (void)
 {
-	int i;
 	// initialize
 	init();
 	
 	// start code from here
+	gpio_set_gpio_pin(RESPONSE_A);
+	gpio_set_gpio_pin(RESPONSE_B);
+	gpio_set_gpio_pin(RESPONSE_C);
+	gpio_enable_pin_interrupt(TEST_A, GPIO_FALLING_EDGE);
+	gpio_enable_pin_interrupt(TEST_B, GPIO_FALLING_EDGE);
+	gpio_enable_pin_interrupt(TEST_C, GPIO_FALLING_EDGE);
 	while(1)
 	{
-		gpio_toggle_pin(LED0_GPIO);
-		printf("tick\n");
-		
-		busy_delay_ms(500);
+		if(Aflag){
+			gpio_clr_gpio_pin(RESPONSE_A);
+			busy_delay_ms(1);
+			gpio_set_gpio_pin(RESPONSE_A);
+			Aflag = 0;
+		}
+		if(Bflag){
+			gpio_clr_gpio_pin(RESPONSE_B);
+			busy_delay_ms(1);
+			gpio_set_gpio_pin(RESPONSE_B);
+			Bflag = 0;
+		}
+		if(Cflag){
+			gpio_clr_gpio_pin(RESPONSE_C);
+			busy_delay_ms(1);
+			gpio_set_gpio_pin(RESPONSE_C);
+			Cflag = 0;
+		}
 	}
 }
