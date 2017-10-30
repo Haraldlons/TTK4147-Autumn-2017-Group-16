cec40cb5999656ff72d1f55028589a5896a675bd
Haraldlons <haraldlons@gmail.com>
2017-10-30 13:01:02 +0100

    add qustions in comments for clarifying stuff

--- miniProject16/client/client.c
+++ miniProject16/client/client.c
@@ -34,8 +34,8 @@ void init(){
 
 void stop(){
 	char send[5];
-	snprintf(send,5,"STOP");
-	send[4] = '\0';
+	snprintf(send,5,"STOP"); //Hvorfor bruke snprintf?
+	send[4] = '\0'; //Dette er kun ét tegn? (ikke to)
 
 	pthread_mutex_lock(&udp);
 	udp_send(&connection, send, 5);
@@ -47,27 +47,27 @@ float get_output(){
 	char recieved[30];
 	char* value;
 	udp_receive(&connection, recieved, 30);
-	value = strstr(recieved,"GET_ACK:");
+	value = strstr(recieved,"GET_ACK:"); //Denne gir alt ETTER 'GET_ACK:'?
 	if(value){
-		value = value+8;
+		value = value+8; //What is this stuff?
 		return(atof(value));
 	}
-	printf("Bad things happened!\n");
+	printf("Bad things happened!\n"); //Maybe change this line?
 	return 0;
 }
 
-void send_get(){
-	char* send = "GET";
+void send_get(){ 
+	char* send = "GET"; 
 
 	pthread_mutex_lock(&udp);
-	udp_send(&connection,send,4);
+	udp_send(&connection,send,4); //Hvorfor må vi noen steder sende '\0' mens andre steder ikke?
 	pthread_mutex_unlock(&udp);
 }
 
 void update_input(float value){
 	char send[13];
 	snprintf(send,13,"SET:%f",value);
-	send[12] = '\0';
+	send[12] = '\0'; 
 
 	pthread_mutex_lock(&udp);
 	udp_send(&connection,send, 13);
@@ -77,21 +77,21 @@ void update_input(float value){
 
 void *pid(){
 	float integral = 0;
-	float error ;
+	float error;
 	float u;
 	float y;
 	struct timespec time_pid;
 	clock_gettime(CLOCK_REALTIME, &time_pid);
 	int i = 0;
-	for(i = 0 ; i < RUNTIME/PERIOD ; i++){
+	for(i = 0 ; i < RUNTIME/PERIOD ; i++){ //Kan man ha en while og heller sjekke om en klokke har nådd 0.5 sek?
 		timespec_add_us(&time_pid, PERIOD);
 		send_get();
 		y = get_output();
 		error = REFERENCE-y;
-		integral = integral + (error*PERIOD/1000000);
+		integral = integral + (error*PERIOD/1000000); //Hvorfor dele på 1 000 000?
 	    u = KP*error + KI*integral;
 		update_input(u);
-		clock_nanosleep(&time_pid);
+		clock_nanosleep(&time_pid); //sove intill?
 	}
 }
 
@@ -99,7 +99,7 @@ int main(){
 	init();
 	pthread_t pid_thread;
 	pthread_create(&pid_thread, NULL, pid, NULL);
-	pthread_join(pid_thread, NULL);
-	stop();
+	pthread_join(pid_thread, NULL); //Join som venter? 
+	stop(); //Hva er det som hindrer funksjonen til å avslutte? 
 	return 1;
 }
